f696f8f6b0d38bd948c4fdf8145b41136e3c75b0
letusfly85 <letusfly95@gmail.com>
2019-05-16 13:50:41 +0900

    :sparkles: add sample PATH /api/health

--- app/server.py
+++ app/server.py
@@ -1,22 +1,16 @@
-from flask import Flask, session, request, jsonify, make_response, render_template
+from flask import Flask, session, request, jsonify, make_response
 from flask_restplus import Api, Resource, fields
 from flask_cors import CORS
 from flask_session import Session
 import requests
 
 import json
-from urllib.parse import parse_qsl
 import sys
 import os
-import sched
-import time
-from datetime import datetime
-import collections
-# from redis import Redis
 
-import hashlib
-import hmac
-import base64
+# from redis import Redis
+# app.config['SESSION_REDIS'] = Redis(host=app.config['REDIS_HOST'], port=app.config['REDIS_PORT'])
+# Session(app)
 
 
 args = sys.argv
@@ -27,20 +21,15 @@ app.config.from_pyfile("config/{}.properties".format(env))
 api = Api(app, version='0.0.1', title='API', description='API')
 
 app.secret_key = app.config['SECRET_KEY']
-app.config['SESSION_REDIS'] = Redis(host=app.config['REDIS_HOST'], port=app.config['REDIS_PORT'])
 app.supports_credentials = True
-Session(app)
 CORS(app, resources={r"/*": {"origins": "*"}})
 
 ns = api.namespace('api')
-pns = api.namespace('')
 
 
-@app.route('/')
-def index():
-    return render_template('index.html')
+@ns.route('/health')
+class HealthCheck(Resource):
 
-
-@app.route('/health')
-def check_health():
-    return jsonify({"status": "ok"})
+    @staticmethod
+    def get():
+        return jsonify({"status": "ok"})
